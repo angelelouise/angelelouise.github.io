a22bcbbdb68dc019c7b46e00129c8c076d9100e9
Angele <angelealst@hotmail.com>
2021-04-03 20:29:33 -0300

    #Alterando para HashRouter

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Landing from "./pages/Landing";
 import Contato from "./pages/Contato";
 import CyExample from "./pages/CyExample";
 import Jogo from "./pages/Jogo";
 function Routes() {
     return(
-        <BrowserRouter>
+        <Router>
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/contato" exact component={Contato} />
                 <Route path="/example-cypress" exact component={CyExample} />
                 <Route path="/jogo" exact component={Jogo} />
             </Switch>
-        </BrowserRouter>
+        </Router>
     );
 
 }
